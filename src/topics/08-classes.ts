0268726e9284521e35a9ba0ea4b2f392785c5386
Alejandro González Reyes <alexglzrys@gmail.com>
2025-11-19 14:17:37 -0600

    comoposición sobre herencia

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -4,10 +4,10 @@ class Person {
     // private address: string;
 
     // Equivalente a lo de arriba, pero más corto. Si se pone "private" o "public" en el constructor, se crea automáticamente la propiedad en la clase.
-    // Esto en Angular se usa mucho, especialmente en los servicios para inyectar dependencias.
     constructor(public name: string, private address: string = 'Unknown') {}
 }
 
+/*
 // Extender una clase
 class Hero extends Person {
     constructor(
@@ -22,9 +22,24 @@ class Hero extends Person {
         super(name, address);
     }
 }
+*/
+
+// Priorizar composición sobre herencia (Inyectar una instancia de Person en Hero)
+// Esto en Angular se usa mucho, especialmente en los servicios para inyectar dependencias.
+class Hero {
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public habilities: string[],
+        public person: Person
+    ) {}
+}
 
 const iroman = new Person('Ironman', 'New York 1234');
-const goku = new Hero('Goku', 40, ['Kamehameha', 'Genkidama'], 'Kakarotto', 'Planet Vegeta');
+// const goku = new Hero('Goku', 40, ['Kamehameha', 'Genkidama'], 'Kakarotto', 'Planet Vegeta');
+
+const personaje = new Person('Kakarotto', 'Planet Vegita');
+const goku = new Hero('Goku', 40, ['Kamehameha', 'Genkidama'], personaje);
 
 console.log(iroman);
 console.log(goku);
