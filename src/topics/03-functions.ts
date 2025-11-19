55577e57b302c97484678eea7922d36ab8c27696
Alejandro González Reyes <alexglzrys@gmail.com>
2025-11-19 11:27:12 -0600

    funciones con objetos como argumentos

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -12,8 +12,31 @@ function multiplyNumbers(
   return firstNumber * base;
 }
 
-const resultAdd = addNumbers(5, 10);
-const resultAddArrow = addNumbersArrow(7, 3);
-const resultMultiply = multiplyNumbers(2);
+// const resultAdd = addNumbers(5, 10);
+// const resultAddArrow = addNumbersArrow(7, 3);
+// const resultMultiply = multiplyNumbers(2);
 
-console.log({ resultAdd, resultAddArrow, resultMultiply });
+// console.log({ resultAdd, resultAddArrow, resultMultiply });
+
+interface Character {
+  name: string;
+  hp: number;
+  showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number): void => {
+  character.hp += amount;
+};
+
+const vegueta: Character = {
+  name: "Vegueta",
+  hp: 50,
+  showHp() {
+    console.log(`Puntos de vida: ${this.hp}`);
+  }
+};
+
+healCharacter(vegueta, 20);
+healCharacter(vegueta, 5);
+
+vegueta.showHp();
